0e0430262e5f3e6ba4d324f645a37504926ffe84
Punit Katiyar <punit.kat@gmail.com>
2021-12-21 17:52:05 +0530

    Update function_2.php

--- function_2.php
+++ function_2.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-    span {
+<style>
+   span {
         color: red;
     }
-    </style>
-</head>
-
-<body>
     <?php
     function shop($data)
     {
@@ -25,12 +13,4 @@
     $res= shop("Punit");
     $output=$res[0].$res[1];
     echo "$temp $output";
-    
-    
-    
     ?>
-
-
-</body>
-
-</html>
