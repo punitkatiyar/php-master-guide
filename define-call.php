80d4d7aa485733a709b360f5446ee2951b4960da
Punit Katiyar <punit.kat@gmail.com>
2021-12-25 11:15:27 +0530

    Add files via upload

---
+++ define-call.php
@@ -0,0 +1,27 @@
+<?php
+say_hello();
+#echo $data;
+// define variable
+$data="variable";
+$data="Punit";
+// call variable
+echo $data;
+// define the function 
+
+function say_hello()
+{
+    // funtion body
+    echo "<h1>Welcome to Function NExt</h1>";
+}
+say_hello();
+// function say_hello()
+// {
+//     // funtion body
+//     echo "<h1>Welcome to Function</h1>";
+// }
+// call the function
+say_hello();
+
+
+
+?>
