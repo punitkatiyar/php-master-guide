7a6c3cc6ef198949aaf3dacb9b2045d4f998a203
Punit Katiyar <punit.kat@gmail.com>
2022-01-01 12:23:06 +0530

    setup

---
+++ 10.project-setup/01.static/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Sample</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <header>
+        <a href="./">Project Sample</a>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="./">Home</a></li>
+            <li><a href="page.php">page</a></li>
+            <li><a href="./">Home</a></li>
+            <li><a href="./">Home</a></li>
+            <li><a href="./">Home</a></li>
+            <li><a href="./">Home</a></li>
+            <li><a href="./">Home</a></li>
+        </ul>
+    </nav>
+    <section class="mid">
+        <div class="mid_left">
+            <article>
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ea?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident facere eius debitis, quas
+                    veniam asperiores distinctio consectetur est at ipsa aliquam aliquid architecto eum consequatur
+                    illum
+                    dolor culpa libero delectus perferendis sit molestiae error. Quibusdam, incidunt reiciendis at quis
+                    nam
+                    veritatis sequi dolor vitae repellat placeat, explicabo praesentium consequatur suscipit ex. Dicta
+                    laborum fugiat fuga magnam rerum id earum doloremque adipisci sed! Veritatis, suscipit. Fuga maxime
+                    sequi dolores amet. Necessitatibus atque rerum quo deleniti tempore aliquam ad laboriosam,
+                    architecto
+                    adipisci, debitis officia itaque, minima quos minus doloribus tempora repellat! Culpa nisi saepe,
+                    eaque
+                    laborum aperiam dolorem obcaecati consectetur assumenda iste vel eum nobis, itaque vero eligendi
+                    voluptate facilis ducimus tenetur. Similique inventore voluptates suscipit recusandae, ab aspernatur
+                    eaque libero autem adipisci eligendi perspiciatis aliquam tenetur corrupti pariatur magnam. A
+                    tempore
+                    consequuntur nemo quod rem omnis unde ullam obcaecati. Nobis quod quam magnam qui unde, alias sed
+                    animi,
+                    aliquid libero id perferendis cupiditate esse, quia beatae modi itaque necessitatibus sit. Cum alias
+                    architecto est distinctio maxime accusantium nobis sapiente modi iure. Itaque ex ab ipsa voluptates
+                    neque sint totam impedit inventore eos, eaque, dignissimos eveniet at veniam odio illo ad voluptate
+                    doloremque, eligendi corrupti ullam dolorum dicta. Vel, facilis quod.</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>HTML layout</li>
+                    <li>HTML template</li>
+                    <li>HTML code</li>
+                </ul>
+            </article>
+            <article>
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ea?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident facere eius debitis, quas
+                    veniam asperiores distinctio consectetur est at ipsa aliquam aliquid architecto eum consequatur
+                    illum
+                    dolor culpa libero delectus perferendis sit molestiae error. Quibusdam, incidunt reiciendis at quis
+                    nam
+                    veritatis sequi dolor vitae repellat placeat, explicabo praesentium consequatur suscipit ex. Dicta
+                    laborum fugiat fuga magnam rerum id earum doloremque adipisci sed! Veritatis, suscipit. Fuga maxime
+                    sequi dolores amet. Necessitatibus atque rerum quo deleniti tempore aliquam ad laboriosam,
+                    architecto
+                    adipisci, debitis officia itaque, minima quos minus doloribus tempora repellat! Culpa nisi saepe,
+                    eaque
+                    laborum aperiam dolorem obcaecati consectetur assumenda iste vel eum nobis, itaque vero eligendi
+                    voluptate facilis ducimus tenetur. Similique inventore voluptates suscipit recusandae, ab aspernatur
+                    eaque libero autem adipisci eligendi perspiciatis aliquam tenetur corrupti pariatur magnam. A
+                    tempore
+                    consequuntur nemo quod rem omnis unde ullam obcaecati. Nobis quod quam magnam qui unde, alias sed
+                    animi,
+                    aliquid libero id perferendis cupiditate esse, quia beatae modi itaque necessitatibus sit. Cum alias
+                    architecto est distinctio maxime accusantium nobis sapiente modi iure. Itaque ex ab ipsa voluptates
+                    neque sint totam impedit inventore eos, eaque, dignissimos eveniet at veniam odio illo ad voluptate
+                    doloremque, eligendi corrupti ullam dolorum dicta. Vel, facilis quod.</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>HTML layout</li>
+                    <li>HTML template</li>
+                    <li>HTML code</li>
+                </ul>
+            </article>
+            <article>
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ea?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident facere eius debitis, quas
+                    veniam asperiores distinctio consectetur est at ipsa aliquam aliquid architecto eum consequatur
+                    illum
+                    dolor culpa libero delectus perferendis sit molestiae error. Quibusdam, incidunt reiciendis at quis
+                    nam
+                    veritatis sequi dolor vitae repellat placeat, explicabo praesentium consequatur suscipit ex. Dicta
+                    laborum fugiat fuga magnam rerum id earum doloremque adipisci sed! Veritatis, suscipit. Fuga maxime
+                    sequi dolores amet. Necessitatibus atque rerum quo deleniti tempore aliquam ad laboriosam,
+                    architecto
+                    adipisci, debitis officia itaque, minima quos minus doloribus tempora repellat! Culpa nisi saepe,
+                    eaque
+                    laborum aperiam dolorem obcaecati consectetur assumenda iste vel eum nobis, itaque vero eligendi
+                    voluptate facilis ducimus tenetur. Similique inventore voluptates suscipit recusandae, ab aspernatur
+                    eaque libero autem adipisci eligendi perspiciatis aliquam tenetur corrupti pariatur magnam. A
+                    tempore
+                    consequuntur nemo quod rem omnis unde ullam obcaecati. Nobis quod quam magnam qui unde, alias sed
+                    animi,
+                    aliquid libero id perferendis cupiditate esse, quia beatae modi itaque necessitatibus sit. Cum alias
+                    architecto est distinctio maxime accusantium nobis sapiente modi iure. Itaque ex ab ipsa voluptates
+                    neque sint totam impedit inventore eos, eaque, dignissimos eveniet at veniam odio illo ad voluptate
+                    doloremque, eligendi corrupti ullam dolorum dicta. Vel, facilis quod.</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>HTML layout</li>
+                    <li>HTML template</li>
+                    <li>HTML code</li>
+                </ul>
+            </article>
+        </div>
+        <div class="mid_right">
+            <marquee>This is aside Section</marquee>
+        </div>
+    </section>
+    <footer>
+        <h4>copyright &copy; 2021-2022 Project Sample </h4>
+    </footer>
+
+</body>
+
+</html>
---
+++ 10.project-setup/02.dynamic/index.php
@@ -0,0 +1,105 @@
+<?php
+include_once('config.php');
+
+include "head.php";
+?>
+<div class="mid_left">
+    <article>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ea?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident facere eius debitis, quas
+            veniam asperiores distinctio consectetur est at ipsa aliquam aliquid architecto eum consequatur
+            illum
+            dolor culpa libero delectus perferendis sit molestiae error. Quibusdam, incidunt reiciendis at quis
+            nam
+            veritatis sequi dolor vitae repellat placeat, explicabo praesentium consequatur suscipit ex. Dicta
+            laborum fugiat fuga magnam rerum id earum doloremque adipisci sed! Veritatis, suscipit. Fuga maxime
+            sequi dolores amet. Necessitatibus atque rerum quo deleniti tempore aliquam ad laboriosam,
+            architecto
+            adipisci, debitis officia itaque, minima quos minus doloribus tempora repellat! Culpa nisi saepe,
+            eaque
+            laborum aperiam dolorem obcaecati consectetur assumenda iste vel eum nobis, itaque vero eligendi
+            voluptate facilis ducimus tenetur. Similique inventore voluptates suscipit recusandae, ab aspernatur
+            eaque libero autem adipisci eligendi perspiciatis aliquam tenetur corrupti pariatur magnam. A
+            tempore
+            consequuntur nemo quod rem omnis unde ullam obcaecati. Nobis quod quam magnam qui unde, alias sed
+            animi,
+            aliquid libero id perferendis cupiditate esse, quia beatae modi itaque necessitatibus sit. Cum alias
+            architecto est distinctio maxime accusantium nobis sapiente modi iure. Itaque ex ab ipsa voluptates
+            neque sint totam impedit inventore eos, eaque, dignissimos eveniet at veniam odio illo ad voluptate
+            doloremque, eligendi corrupti ullam dolorum dicta. Vel, facilis quod.</p>
+        <ul>
+            <li>HTML</li>
+            <li>HTML layout</li>
+            <li>HTML template</li>
+            <li>HTML code</li>
+        </ul>
+    </article>
+    <article>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ea?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident facere eius debitis, quas
+            veniam asperiores distinctio consectetur est at ipsa aliquam aliquid architecto eum consequatur
+            illum
+            dolor culpa libero delectus perferendis sit molestiae error. Quibusdam, incidunt reiciendis at quis
+            nam
+            veritatis sequi dolor vitae repellat placeat, explicabo praesentium consequatur suscipit ex. Dicta
+            laborum fugiat fuga magnam rerum id earum doloremque adipisci sed! Veritatis, suscipit. Fuga maxime
+            sequi dolores amet. Necessitatibus atque rerum quo deleniti tempore aliquam ad laboriosam,
+            architecto
+            adipisci, debitis officia itaque, minima quos minus doloribus tempora repellat! Culpa nisi saepe,
+            eaque
+            laborum aperiam dolorem obcaecati consectetur assumenda iste vel eum nobis, itaque vero eligendi
+            voluptate facilis ducimus tenetur. Similique inventore voluptates suscipit recusandae, ab aspernatur
+            eaque libero autem adipisci eligendi perspiciatis aliquam tenetur corrupti pariatur magnam. A
+            tempore
+            consequuntur nemo quod rem omnis unde ullam obcaecati. Nobis quod quam magnam qui unde, alias sed
+            animi,
+            aliquid libero id perferendis cupiditate esse, quia beatae modi itaque necessitatibus sit. Cum alias
+            architecto est distinctio maxime accusantium nobis sapiente modi iure. Itaque ex ab ipsa voluptates
+            neque sint totam impedit inventore eos, eaque, dignissimos eveniet at veniam odio illo ad voluptate
+            doloremque, eligendi corrupti ullam dolorum dicta. Vel, facilis quod.</p>
+        <ul>
+            <li>HTML</li>
+            <li>HTML layout</li>
+            <li>HTML template</li>
+            <li>HTML code</li>
+        </ul>
+    </article>
+    <article>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ea?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores provident facere eius debitis, quas
+            veniam asperiores distinctio consectetur est at ipsa aliquam aliquid architecto eum consequatur
+            illum
+            dolor culpa libero delectus perferendis sit molestiae error. Quibusdam, incidunt reiciendis at quis
+            nam
+            veritatis sequi dolor vitae repellat placeat, explicabo praesentium consequatur suscipit ex. Dicta
+            laborum fugiat fuga magnam rerum id earum doloremque adipisci sed! Veritatis, suscipit. Fuga maxime
+            sequi dolores amet. Necessitatibus atque rerum quo deleniti tempore aliquam ad laboriosam,
+            architecto
+            adipisci, debitis officia itaque, minima quos minus doloribus tempora repellat! Culpa nisi saepe,
+            eaque
+            laborum aperiam dolorem obcaecati consectetur assumenda iste vel eum nobis, itaque vero eligendi
+            voluptate facilis ducimus tenetur. Similique inventore voluptates suscipit recusandae, ab aspernatur
+            eaque libero autem adipisci eligendi perspiciatis aliquam tenetur corrupti pariatur magnam. A
+            tempore
+            consequuntur nemo quod rem omnis unde ullam obcaecati. Nobis quod quam magnam qui unde, alias sed
+            animi,
+            aliquid libero id perferendis cupiditate esse, quia beatae modi itaque necessitatibus sit. Cum alias
+            architecto est distinctio maxime accusantium nobis sapiente modi iure. Itaque ex ab ipsa voluptates
+            neque sint totam impedit inventore eos, eaque, dignissimos eveniet at veniam odio illo ad voluptate
+            doloremque, eligendi corrupti ullam dolorum dicta. Vel, facilis quod.</p>
+        <ul>
+            <li>HTML</li>
+            <li>HTML layout</li>
+            <li>HTML template</li>
+            <li>HTML code</li>
+        </ul>
+    </article>
+</div>
+<div class="mid_right">
+    <marquee>This is aside Section</marquee>
+    <img src="src/image/php-include-require.png" width="100%">
+</div>
+
+<?php
+include 'footer.php';
+?>
