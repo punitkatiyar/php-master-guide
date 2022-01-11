ab8bf203fe894a7fcb16636e5b7f7127298eae2a
Punit Katiyar <punit.kat@gmail.com>
2022-01-11 10:35:35 +0530

    Update variable.php

--- 06.variable-datatype/variable.php
+++ 06.variable-datatype/variable.php
@@ -1,1 +1,24 @@
+<title>PHP Variable</title>
+<?php
+$heading="<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, est.</h1>";
+$para="<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>";
+$heading="<h2>WELCOKME TO php VARIABLE</h2>";
+$HEADING="<h3>Ducat India</h3>";
+echo $heading;
+echo $para;
+//name convension
+$mydata=100;
+$MYDATA=101;
+$myData=102;
+//$my data=103;
+//$my-data=104;
+$my_data=105;
+$mydata1=106;
+//$1mydata=107;
+$_mydata=108;  // can not create
+$__mydata=109; // can not create
+//$@mydata=110;
 
+
+
+?>
