bbfa2662af67642ddbd751c286f6e197e5d5440e
Punit Katiyar <punit.kat@gmail.com>
2022-08-03 19:02:45 +0530

    Create index.php

---
+++ 10.function/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'config.php';
+require_once 'function.php';
+head("My Website");
+alink("index","home");
+alink("page1");
+alink("page2");
+alink("page3");
+?>
+
+
+
+
+<script>
+    document.getElementById("home").innerHTML="Home";
+</script>
+
+
+</body>
+
+</html>
