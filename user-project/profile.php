d40f5e12879f9470fad7176ef87e265509952b3b
Punit Katiyar <punit.kat@gmail.com>
2022-09-01 18:07:19 +0530

    Add files via upload

--- user-project/profile.php
+++ user-project/profile.php
@@ -2,6 +2,14 @@
 require_once 'database.php';
 $query="SELECT * FROM `student`";   
 $data=mysqli_query($dbcon,$query);
+
+if(isset($_POST['supdate']))
+{
+    extract($_POST);
+    $query="UPDATE `student` SET `sname`='$sname',`semail`='$semail',`scourse`='$scourse',`smobile`='$smobile',`scity`='$scity' WHERE `sid`='$hid'";   
+    mysqli_query($dbcon,$query);
+    header("location:profile.php");
+}
 ?>
 <html>
 <head>  
@@ -10,22 +18,29 @@ $data=mysqli_query($dbcon,$query);
 </head>
 <body>
 <?php
+require_once "head.php";
 if(@$_GET['action']=='update')
 {
-    // echo "<h1>".$_GET['id']."</h1>";
+    $id=$_GET['id'];
+    echo "<h1 class='text-center'>".$_GET['id']."</h1>";
+    $query="SELECT * FROM `student` WHERE `sid`='$id'";   
+    $data=mysqli_query($dbcon,$query);
+    $ures=mysqli_fetch_array($data);
     ?>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-4">
+            <div class="col-4 bg-primary my-2 p-5">
+                <div class="bg-white h4 p-2 text-primary">Please Update data..</div>
                 <form method="post">
-                    <input type="text" name="sname" placeholder="Enter Your name" class="form-control mb-2" required>
-                    <input type="email" name="semail" placeholder="Enter Your Email" class="form-control mb-2" required>
-                    <input type="text" name="scourse" placeholder="Enter Your Course" class="form-control mb-2"
+                    <input type="hidden" name="hid" value="<?php echo $id ?>">
+                    <input type="text" name="sname" placeholder="Enter Your name" value="<?php echo $ures['sname'];?>" class="form-control my-2" required>
+                    <input type="email" name="semail" placeholder="Enter Your Email" value="<?php echo $ures['semail'];?>" class="form-control mb-2" required>
+                    <input type="text" name="scourse" placeholder="Enter Your Course" value="<?php echo $ures['scourse'];?>" class="form-control mb-2"
                         required>
-                    <input type="number" name="smobile" placeholder="Enter Your Mobile" class="form-control mb-2"
+                    <input type="text" name="smobile" placeholder="Enter Your Mobile" value="<?php echo $ures['smobile'];?>" class="form-control mb-2"
                         required>
-                    <input type="text" name="scity" placeholder="Enter Your City" class="form-control mb-2" required>
-                    <input type="submit" name="sreg" value="Register" class="form-control mb-2">
+                    <input type="text" name="scity" placeholder="Enter Your City" class="form-control mb-2" value="<?php echo $ures['scity'];?>" required>
+                    <input type="submit" name="supdate" value="Update Data" class="form-control mb-2">
                 </form>
             </div>
         </div>
@@ -36,25 +51,27 @@ if(@$_GET['action']=='update')
 }
 else{
     ?>
-    <table border="1" width="90%" align="center" cellpadding="10px">
+    <div class="container">
+    <table align="center" class="table table-hover">
     <?php
     while($res=mysqli_fetch_array($data))
     {
         ?>
         <tr>
-            <td><?php echo $res['sid'];?></td>
+            <td class="bg-info"><?php echo $res['sid'];?></td>
             <td><?php echo $res['sname'];?></td>
             <td><?php echo $res['semail'];?></td>
             <td><?php echo $res['spass'];?></td>
             <td><?php echo $res['scourse'];?></td>
             <td><?php echo $res['smobile'];?></td>
             <td><?php echo $res['scity'];?></td>
-            <td><a href="?id=<?php echo $res['sid'];?>&action=update">Update data</a></td>
+            <td><a class="btn btn-primary" href="?id=<?php echo $res['sid'];?>&action=update">Update data</a></td>
         </tr>
         <?php
     }
     ?>
 </table>
+</div>
 
 
     <?php
